df1a6f7650df34dc8787f540e798380e45749a00
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-18 12:56:13 -0300

    121. Programando Tela de Cadastro do App

--- app/src/main/java/com/exemplo/casaportemporada/activity/autenticacao/CriarContaActivity.java
+++ app/src/main/java/com/exemplo/casaportemporada/activity/autenticacao/CriarContaActivity.java
@@ -2,12 +2,27 @@ package com.exemplo.casaportemporada.activity.autenticacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.exemplo.casaportemporada.R;
+import com.exemplo.casaportemporada.activity.MainActivity;
+import com.exemplo.casaportemporada.helper.FirebaseHelper;
+import com.exemplo.casaportemporada.model.Usuario;
 
 public class CriarContaActivity extends AppCompatActivity {
+
+    private EditText edit_nome;
+    private EditText edit_email;
+    private EditText edit_telefone;
+    private EditText edit_senha;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,70 @@ public class CriarContaActivity extends AppCompatActivity {
     private void iniciarComponentes() {
         TextView text_titulo = findViewById(R.id.text_titulo);
         text_titulo.setText("Crie sua conta");
+
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_telefone = findViewById(R.id.edit_telefone);
+        edit_senha = findViewById(R.id.edit_senha);
+
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void validarDados(View view) {
+        String nome = edit_nome.getText().toString();
+        String email = edit_email.getText().toString();
+        String telefone = edit_telefone.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        if(!nome.isEmpty()) {
+            if(!email.isEmpty()) {
+                if(!telefone.isEmpty()) {
+                    if(!senha.isEmpty()) {
+                        progressBar.setVisibility(View.VISIBLE);
+
+                        Usuario usuario = new Usuario();
+                        usuario.setNome(nome);
+                        usuario.setEmail(email);
+                        usuario.setTelefone(telefone);
+                        usuario.setSenha(senha);
+
+                        cadastrarUsuario(usuario);
+                    } else {
+                        edit_senha.requestFocus();
+                        edit_senha.setError("Informe sua senha");
+                    }
+                } else {
+                    edit_telefone.requestFocus();
+                    edit_telefone.setError("Informe seu telefone");
+                }
+            } else {
+                edit_email.requestFocus();
+                edit_email.setError("Informe seu e-Mail");
+            }
+        } else {
+            edit_nome.requestFocus();
+            edit_nome.setError("Informe seu nome");
+        }
+    }
+
+    private void cadastrarUsuario(Usuario usuario) {
+        FirebaseHelper.getAuth().createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                String idUser = task.getResult().getUser().getUid();
+                usuario.setId(idUser);
+
+                usuario.salvar();
+
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+            } else {
+                String error = task.getException().getMessage();
+                Toast.makeText(this, "Erro ao cadastrar usuário. Motivo: " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
