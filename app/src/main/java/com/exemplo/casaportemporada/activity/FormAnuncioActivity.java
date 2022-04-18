5abae0781a06821ae0dd8bbf8c16d91c963cd7b3
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-18 19:47:14 -0300

    126. Configurando Permissão Para Acessar a Galeria

--- app/src/main/java/com/exemplo/casaportemporada/activity/FormAnuncioActivity.java
+++ app/src/main/java/com/exemplo/casaportemporada/activity/FormAnuncioActivity.java
@@ -1,16 +1,33 @@
 package com.exemplo.casaportemporada.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.exemplo.casaportemporada.R;
 import com.exemplo.casaportemporada.model.Produto;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class FormAnuncioActivity extends AppCompatActivity {
+
+    public static final int REQUEST_GALERIA = 100;
+
+    private ImageView img_anuncio;
+    private String caminhoImagem;
+    private Bitmap imagem;
 
     private EditText edit_titulo;
     private EditText edit_descricao;
@@ -56,11 +73,11 @@ public class FormAnuncioActivity extends AppCompatActivity {
         String banheiro = edit_banheiro.getText().toString();
         String garagem = edit_garagem.getText().toString();
 
-        if(!titulo.isEmpty()) {
-            if(!descricao.isEmpty()) {
-                if(!quarto.isEmpty()) {
-                    if(!banheiro.isEmpty()) {
-                        if(!garagem.isEmpty()) {
+        if (!titulo.isEmpty()) {
+            if (!descricao.isEmpty()) {
+                if (!quarto.isEmpty()) {
+                    if (!banheiro.isEmpty()) {
+                        if (!garagem.isEmpty()) {
                             Produto produto = new Produto();
                             produto.setTitulo(titulo);
                             produto.setDescricao(descricao);
@@ -88,6 +105,40 @@ public class FormAnuncioActivity extends AppCompatActivity {
             edit_titulo.requestFocus();
             edit_titulo.setError("Informe um título");
         }
+    }
+
+    public void verificarPermissaoGaleria(View view) {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(FormAnuncioActivity.this, "Permissao negada", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        showDialogPermissaoGaleria(permissionListener, new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        });
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_GALERIA);
+    }
+
+    private void showDialogPermissaoGaleria(PermissionListener listener, String[] permissoes) {
+        TedPermission.create()
+                .setPermissionListener(listener)
+                .setDeniedTitle("Permissões negadas")
+                .setDeniedMessage("Você negou a permissão para acessar a galeria do dispositivo. Deseja permitir?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(permissoes)
+                .check();
     }
 
 }
