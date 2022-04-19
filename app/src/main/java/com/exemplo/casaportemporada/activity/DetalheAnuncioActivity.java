d7948cdefd79c2bc87b3f9608682f40ca7a0f07c
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-19 12:57:20 -0300

    137. Criando Layout Tela de Detalhes dos Anúncios

---
+++ app/src/main/java/com/exemplo/casaportemporada/activity/DetalheAnuncioActivity.java
@@ -0,0 +1,16 @@
+package com.exemplo.casaportemporada.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exemplo.casaportemporada.R;
+
+public class DetalheAnuncioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_anuncio);
+    }
+}
