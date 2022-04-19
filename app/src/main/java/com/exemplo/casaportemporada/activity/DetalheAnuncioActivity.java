30e9ecc8767f005b4c2ff874f6f7c1f6e9e6ecc3
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-19 15:28:16 -0300

    140. Efetuando Chamandas Para Contatos dos Anúncios

--- app/src/main/java/com/exemplo/casaportemporada/activity/DetalheAnuncioActivity.java
+++ app/src/main/java/com/exemplo/casaportemporada/activity/DetalheAnuncioActivity.java
@@ -1,17 +1,26 @@
 package com.exemplo.casaportemporada.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.exemplo.casaportemporada.R;
+import com.exemplo.casaportemporada.helper.FirebaseHelper;
 import com.exemplo.casaportemporada.model.Anuncio;
+import com.exemplo.casaportemporada.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class DetalheAnuncioActivity extends AppCompatActivity {
@@ -27,6 +36,8 @@ public class DetalheAnuncioActivity extends AppCompatActivity {
 
     private Anuncio anuncio;
 
+    private Usuario usuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +46,41 @@ public class DetalheAnuncioActivity extends AppCompatActivity {
         iniciarComponentes();
 
         anuncio = (Anuncio) getIntent().getSerializableExtra("anuncio");
+        
+        recuperaAnunciante();
 
         configDados();
 
         configCliques();
+    }
+
+    public void ligar(View view) {
+        if(usuario != null) {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + usuario.getTelefone()));
+
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Carregando informações. Aguarde...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void recuperaAnunciante() {
+        DatabaseReference reference = FirebaseHelper.getDatabaseReference()
+                .child("usuarios")
+                .child(anuncio.getIdUsuario());
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usuario = snapshot.getValue(Usuario.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void configDados() {
