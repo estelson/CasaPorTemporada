5bac348949095f9219ef0b476f8b1e0daf097924
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-19 18:23:34 -0300

    142. Programando Tela de Filtros Parte 1

--- app/src/main/java/com/exemplo/casaportemporada/activity/FiltrarAnunciosActivity.java
+++ app/src/main/java/com/exemplo/casaportemporada/activity/FiltrarAnunciosActivity.java
@@ -3,15 +3,65 @@ package com.exemplo.casaportemporada.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.exemplo.casaportemporada.R;
 
 public class FiltrarAnunciosActivity extends AppCompatActivity {
 
+    private TextView text_quarto;
+    private TextView text_banheiro;
+    private TextView text_garagem;
+
+    private SeekBar sb_quarto;
+    private SeekBar sb_banheiro;
+    private SeekBar sb_garagem;
+
+    private int qtd_quarto;
+    private int qtd_banheiro;
+    private int qtd_garagem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtrar_anuncios);
+
+        iniciarComponentes();
+
+        configCliques();
+    }
+
+    private void configCliques() {
+        findViewById(R.id.ib_voltar).setOnClickListener(view -> {
+            finish();
+        });
+    }
+
+    private void iniciarComponentes() {
+        TextView text_titulo = findViewById(R.id.text_titulo);
+        text_titulo.setText("Filtrar");
+
+        text_quarto = findViewById(R.id.text_quarto);
+        text_banheiro = findViewById(R.id.text_banheiro);
+        text_garagem = findViewById(R.id.text_garagem);
+
+        sb_quarto = findViewById(R.id.sb_quarto);
+        sb_banheiro = findViewById(R.id.sb_banheiro);
+        sb_garagem = findViewById(R.id.sb_garagem);
+    }
+
+    public void limparFiltro(View view) {
+        qtd_quarto = 0;
+        qtd_banheiro = 0;
+        qtd_garagem = 0;
+
+        sb_quarto.setProgress(0);
+        sb_banheiro.setProgress(0);
+        sb_garagem.setProgress(0);
+
+        finish();
     }
 
 }
---
+++ app/src/main/java/com/exemplo/casaportemporada/model/Filtro.java
@@ -0,0 +1,32 @@
+package com.exemplo.casaportemporada.model;
+
+public class Filtro {
+
+    private int qtdQuarto;
+    private int qtdBanheiro;
+    private int qtdGaragem;
+
+    public int getQtdQuarto() {
+        return qtdQuarto;
+    }
+
+    public void setQtdQuarto(int qtdQuarto) {
+        this.qtdQuarto = qtdQuarto;
+    }
+
+    public int getQtdBanheiro() {
+        return qtdBanheiro;
+    }
+
+    public void setQtdBanheiro(int qtdBanheiro) {
+        this.qtdBanheiro = qtdBanheiro;
+    }
+
+    public int getQtdGaragem() {
+        return qtdGaragem;
+    }
+
+    public void setQtdGaragem(int qtdGaragem) {
+        this.qtdGaragem = qtdGaragem;
+    }
+}
